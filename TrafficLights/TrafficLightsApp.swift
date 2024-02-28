e901b8653c8baac68ee6871455d3a0d3ee6aaecf
anton_penkov <anton.penkov1@gmail.com>
2024-02-28 20:01:38 +0100

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Антон Пеньков on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
